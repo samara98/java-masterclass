c26f760b5262b0dff8b2ecd877ebda77eb62f96a
Sulthon Abdul Malik <sulthon.almalik@gmail.com>
2020-04-19 04:46:58 +0700

    Perfect Number

---
+++ Exc22_PerfectNumber/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println(PerfectNumber.isPerfectNumber(6));
+        System.out.println(PerfectNumber.isPerfectNumber(28));
+        System.out.println(PerfectNumber.isPerfectNumber(5));
+        System.out.println(PerfectNumber.isPerfectNumber(-1));
+    }
+}
