f0250b132d0a0b57a58792fd6957cf2dc310a5ae
Sulthon Abdul Malik <sulthon.almalik@gmail.com>
2020-04-18 18:59:23 +0700

    Leap Year

---
+++ Exc04_LeapYear/src/LeapYear.java
@@ -0,0 +1,15 @@
+public class LeapYear {
+	public static boolean isLeapYear(int year) {
+		if ((year > 0) && (year < 10000)) {
+			if (year % 4 == 0) {
+				if (year % 100 == 0) {
+					boolean isIt = (year % 400 == 0) ? true : false;
+					return isIt;
+				}
+				return true;
+			}
+			return false;
+		}
+		return false;
+	}
+}
---
+++ Exc04_LeapYear/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println(LeapYear.isLeapYear(-1600));
+        System.out.println(LeapYear.isLeapYear(1600));
+        System.out.println(LeapYear.isLeapYear(2017));
+        System.out.println(LeapYear.isLeapYear(2000));
+        System.out.println(LeapYear.isLeapYear(200));
+    }
+}
