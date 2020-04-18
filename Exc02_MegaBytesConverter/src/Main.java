a4788236b3401b64856dc53a30f20ff2baf61a0b
Sulthon Abdul Malik <sulthon.almalik@gmail.com>
2020-04-18 18:58:53 +0700

    MegaBytes Converter

---
+++ Exc02_MegaBytesConverter/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        MegaBytesConverter.printMegaBytesAndKiloBytes(2500);
+        MegaBytesConverter.printMegaBytesAndKiloBytes(-1024);
+        MegaBytesConverter.printMegaBytesAndKiloBytes(5000);
+    }
+}
