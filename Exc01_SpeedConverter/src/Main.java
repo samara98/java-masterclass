a175a7040fc1d3e0b5cd45dc82af42ea58dd83b7
Sulthon Abdul Malik <sulthon.almalik@gmail.com>
2020-04-18 18:58:30 +0700

    Speed Converter

---
+++ Exc01_SpeedConverter/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        SpeedConverter.printConversion(1.5);
+        SpeedConverter.printConversion(10.25);
+        SpeedConverter.printConversion(-5.6);
+        SpeedConverter.printConversion(25.42);
+        SpeedConverter.printConversion(75.114);
+    }
+}
