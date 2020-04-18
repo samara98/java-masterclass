3b28019d1e2e70b4ffbee057fe6566e96932d7a6
Sulthon Abdul Malik <sulthon.almalik@gmail.com>
2020-04-19 02:32:08 +0700

    Even Digit Sum

---
+++ Exc17_EvenDigitSum/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println(EvenDigitSum.getEvenDigitSum(123456789));
+        System.out.println(EvenDigitSum.getEvenDigitSum(252));
+        System.out.println(EvenDigitSum.getEvenDigitSum(-22));
+    }
+}
