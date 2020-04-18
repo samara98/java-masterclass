f432762067b0d81b4e9f15f107096a5900e3ba4c
Sulthon Abdul Malik <sulthon.almalik@gmail.com>
2020-04-18 18:59:56 +0700

    Decimal Comparator

---
+++ Exc05_DecimalComparator/src/DecimalComparator.java
@@ -0,0 +1,7 @@
+public class DecimalComparator {
+	public static boolean areEqualByThreeDecimalPlaces(double n1, double n2) {
+		n1 = (int)(n1 * 1000);
+		n2 = (int)(n2 * 1000);
+		return (n1 == n2);
+	}
+}
