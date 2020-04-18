87f233ca11f10b9d8699511beb43f2e4310fe39e
Sulthon Abdul Malik <sulthon.almalik@gmail.com>
2020-04-18 19:04:11 +0700

    Number in Word

---
+++ Exc12_NumberInWord/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        NumberInWord.printNumberInWord(0);
+        NumberInWord.printNumberInWord(9);
+        NumberInWord.printNumberInWord(10);
+        NumberInWord.printNumberInWord(-10);
+    }
+}
