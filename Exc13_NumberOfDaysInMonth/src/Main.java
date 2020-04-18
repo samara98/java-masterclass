ad1169ee7cba8412f02eec19b745bec898793f63
Sulthon Abdul Malik <sulthon.almalik@gmail.com>
2020-04-18 23:22:13 +0700

    Number Of Days In Month

---
+++ Exc13_NumberOfDaysInMonth/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println(NumberOfDaysInMonth.getDaysInMonth(1, 2020));
+        System.out.println(NumberOfDaysInMonth.getDaysInMonth(2, 2020));
+        System.out.println(NumberOfDaysInMonth.getDaysInMonth(2, 2018));
+        System.out.println(NumberOfDaysInMonth.getDaysInMonth(-1, 2020));
+        System.out.println(NumberOfDaysInMonth.getDaysInMonth(1, -2020));
+    }
+}
