def5e19f2381c793114874079a94b81b69c00970
Sulthon Abdul Malik <sulthon.almalik@gmail.com>
2020-04-20 06:54:35 +0700

    Diagonal Start

---
+++ Exc26_DiagonalStar/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        DiagonalStar.printSquareStar(5);
+        DiagonalStar.printSquareStar(8);
+        DiagonalStar.printSquareStar(3);
+    }
+}
