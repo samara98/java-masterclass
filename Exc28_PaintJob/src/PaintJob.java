e9cfc52cc27d05422533ea6488b40ac72e7c4c6e
Sulthon Abdul Malik <sulthon.almalik@gmail.com>
2020-04-20 18:01:12 +0700

    Paint Job

---
+++ Exc28_PaintJob/src/PaintJob.java
@@ -0,0 +1,23 @@
+public class PaintJob {
+	public static int getBucketCount(double width, double height, double areaPerBucket, int extraBuckets) {
+		if (width <= 0 || height <= 0 || areaPerBucket <= 0 || extraBuckets < 0) {
+			return -1;
+		}
+		int result = (int) Math.ceil((width * height) / areaPerBucket) - extraBuckets;
+		return result;
+	}
+	public static int getBucketCount(double width, double height, double areaPerBucket) {
+		if (width <= 0 || height <= 0 || areaPerBucket <= 0) {
+			return -1;
+		}
+		int result = (int) Math.ceil((width * height) / areaPerBucket);
+		return result;
+	}
+	public static int getBucketCount(double area, double areaPerBucket) {
+		if (area <= 0 || areaPerBucket <= 0) {
+			return -1;
+		}
+		int result = (int) Math.ceil(area / areaPerBucket);
+		return result;
+	}
+}
