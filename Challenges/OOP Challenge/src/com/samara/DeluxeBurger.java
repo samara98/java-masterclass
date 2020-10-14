88d8577974341975340ada01c40b27b7fa8c6749
Sulthon Abdul Malik <sulthon.almalik@gmail.com>
2020-10-14 11:27:42 +0700

    OOP master challenge

---
+++ Challenges/OOP Challenge/src/com/samara/DeluxeBurger.java
@@ -0,0 +1,31 @@
+package com.samara;
+
+public class DeluxeBurger extends Hamburger {
+	// constructors
+	public DeluxeBurger() {
+		super("Deluxe", "Sausage & Bacon", 14.54, "White");
+		super.addHamburgerAddition1("Chips", 2.75);
+		super.addHamburgerAddition2("Drink", 1.81);
+	}
+
+	// overriding
+	@Override
+	public void addHamburgerAddition1(String name, double price) {
+		System.out.println("Cannot not add additional items to a deluxe burger");
+	}
+
+	@Override
+	public void addHamburgerAddition2(String name, double price) {
+		System.out.println("Cannot not add additional items to a deluxe burger");
+	}
+
+	@Override
+	public void addHamburgerAddition3(String name, double price) {
+		System.out.println("Cannot not add additional items to a deluxe burger");
+	}
+
+	@Override
+	public void addHamburgerAddition4(String name, double price) {
+		System.out.println("Cannot not add additional items to a deluxe burger");
+	}
+}
